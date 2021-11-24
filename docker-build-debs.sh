9eb6254b5a533d323ff7864262e19c237cb814ff
Stephan Enderlein <stephan@freifunk-dresden.de>
2021-11-24 17:01:01 +0100

    add debian:11

--- docker-build-debs.sh
+++ docker-build-debs.sh
@@ -4,7 +4,7 @@ echo "unsetting env-variable \$DOCKER_HOST"
 unset DOCKER_HOST
 
 
-for linux in ubuntu:18.04 ubuntu:20.04 debian:9 debian:10
+for linux in ubuntu:18.04 ubuntu:20.04 debian:9 debian:10 debian:11
 do
 	docker run -it --rm -w /mnt/ -v $PWD:/mnt/ ${linux} bash -c "./tools/install-deps.sh; ./tools/build-bmxd-deb.sh $(id -u) $(id -g)"
 done
--- tools/install-deps.sh
+++ tools/install-deps.sh
@@ -7,7 +7,7 @@ apt update -y
 DEBIAN_FRONTEND=noninteractive apt install -y --no-install-recommends tzdata
 
 apt install -y git gettext curl wget time rsync jq \
-	nodejs build-essential devscripts debhelper dh-systemd python dh-python libssl-dev libncurses5-dev unzip gawk zlib1g-dev subversion gcc-multilib flex \
+	nodejs build-essential devscripts debhelper python libssl-dev libncurses5-dev unzip gawk zlib1g-dev subversion gcc-multilib flex \
 	libjson-c-dev clang lua5.1 liblua5.1-dev cmake
 
 exit 0
